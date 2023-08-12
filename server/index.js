39f9dbed8fa8e3bdd5ec6a9f11add003259d074e
Mark Baynes <mark@jazzpiano.co.nz>
2023-08-13 11:10:11 +1200

    prepare for deployment

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ import server from './server.js'
 
 // const port = 3000
 const port = process.env.PORT || 3000
-const url = `http://localhost:${port}`
+// const url = `http://localhost:${port}`
 
 server.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
-  console.log(`Visit here: ${url}`)
+  // console.log(`Visit here: ${url}`)
 })
